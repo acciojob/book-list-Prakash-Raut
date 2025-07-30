6fa50e2037596df792e69958452b68d59575c25b
Prakash Raut <prakash_raut@outlook.com>
2025-07-30 13:47:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your JS code here. If required.
+document.getElementById('book-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const isbn = document.getElementById('isbn').value;
+
+  if (title === '' || author === '' || isbn === '') {
+    alert('Please fill in all fields');
+    return;
+  }
+
+  addBookToList(title, author, isbn);
+
+  clearFields();
+});
+
+document.getElementById('book-list').addEventListener('click', function (e) {
+  if (e.target.classList.contains('delete')) {
+    e.target.parentElement.parentElement.remove();
+  }
+});
+
+
+function addBookToList(title, author, isbn) {
+  const list = document.getElementById('book-list');
+
+  const row = document.createElement('tr');
+
+  row.innerHTML = `
+        <td>${title}</td>
+        <td>${author}</td>
+        <td>${isbn}</td>
+        <td><button class="delete">X</a></td>
+    `;
+
+  list.appendChild(row);
+}
+
+function clearFields() {
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('isbn').value = '';
+}
